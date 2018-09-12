1249c73a2452e755bf4962bdc6b85cfcaeadbb4e
Gupta-Abhishek <Gupta.Abhishek2@bcg.com>
2018-09-12 10:44:31 +0530

    Added code to Create dataframe

--- first.py
+++ first.py
@@ -17,3 +17,12 @@ x=2
 y=4
 
 print("Sum of two numbers is {}".format(x+y))
+
+
+#creating pandas DataFrame
+import pandas as pd
+
+data=pd.DataFrame.from_dict(data={'A':{0:1,1:2,2:3},
+                             'B':{0:3,1:4,2:5},
+                             'C':{0:7,1:8,2:9}
+                             })
